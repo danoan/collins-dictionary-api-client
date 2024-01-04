97e640e5312ce599d5cc5f58862fa6dc532f409d
Daniel Martins Antunes <danoan2008@gmail.com>
2024-01-04 11:34:13 +0100

    Setup tests

--- tests/core/api/test_api.py
+++ tests/core/api/test_api.py
@@ -1,5 +1,12 @@
+"""
+The goal of these tests is to make sure that the API endpoints are working.
+We are not particularly interested in the content of the response, but
+actually in the success of the request.
+"""
+
 from danoan.dictionaries.collins.core import api, model
 
+from bs4 import BeautifulSoup
 import os
 from pathlib import Path
 import pytest
@@ -30,16 +37,18 @@ def entrypoint(pytestconfig):
 )
 def test_best_matching(language, word, entrypoint, secret_key):
     response = api.get_best_matching(entrypoint, secret_key, language, word)
+    assert response.status_code == 200
 
-    expected_response = SCRIPT_FOLDER / "expected" / "get_best_matching.xml"
-    # with open(expected_response, "w") as f:
-    #     f.write(response.text)
+    xml_response = BeautifulSoup(response.text, "xml")
 
-    with open(expected_response, "r") as f:
-        for expected_line, output_line in zip(
-            f.readlines(), response.text.splitlines()
-        ):
-            assert expected_line.strip() == output_line.strip()
+    print(xml_response.find("dictionaryCode"))
+    print(xml_response.find("entryLabel"))
+
+    assert (
+        str(xml_response.find("dictionaryCode"))
+        == "<dictionaryCode>english</dictionaryCode>"
+    )
+    assert str(xml_response.find("entryLabel")) == "<entryLabel>legitim</entryLabel>"
 
 
 @pytest.mark.parametrize(
@@ -48,13 +57,15 @@ def test_best_matching(language, word, entrypoint, secret_key):
 )
 def test_get_entry(language, entry_id, entrypoint, secret_key):
     response = api.get_entry(entrypoint, secret_key, language, entry_id)
+    assert response.status_code == 200
 
-    expected_response = SCRIPT_FOLDER / "expected" / "get_entry.xml"
-    # with open(expected_response, "w") as f:
-    #     f.write(response.text)
+    xml_response = BeautifulSoup(response.text, "xml")
 
-    with open(expected_response, "r") as f:
-        for expected_line, output_line in zip(
-            f.readlines(), response.text.splitlines()
-        ):
-            assert expected_line.strip() == output_line.strip()
+    print(xml_response.find("dictionaryCode"))
+    print(xml_response.find("entryLabel"))
+
+    assert (
+        str(xml_response.find("dictionaryCode"))
+        == "<dictionaryCode>english</dictionaryCode>"
+    )
+    assert str(xml_response.find("entryLabel")) == "<entryLabel>legitim</entryLabel>"
